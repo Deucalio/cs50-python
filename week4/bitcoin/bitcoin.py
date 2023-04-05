1d687e393139deefb38354111ac8ea06d4f28aa8
Deucalio <hjamad89@gmail.com>
2023-04-05 23:29:34 +0500

    bitcoint

---
+++ week4/bitcoin/bitcoin.py
@@ -0,0 +1,17 @@
+import requests
+import sys
+
+if (len(sys.argv) < 2):
+    sys.exit("Missing command-line argument")
+
+number_of_bitcoins = sys.argv[1]
+if number_of_bitcoins.isalpha():
+    sys.exit("Command-line argument is not a number")
+
+res = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+res = res.json()
+rate = res['bpi']['USD']['rate_float']
+
+cost = rate * float(number_of_bitcoins)
+output = "$"+"{:,}".format(cost)
+print(output)
